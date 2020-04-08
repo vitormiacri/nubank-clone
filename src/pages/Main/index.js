e48d78a5a8c3e49f7214119bb802ee65654f04ed
Vitor Morelli <vitao.miacri@gmail.com>
2020-04-08 07:49:05 -0300

    Main component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Card from '~/components/Card';
+
+import { Container } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Header />
+      <Card />
+      <Tabs />
+    </Container>
+  );
 }
